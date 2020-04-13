a5084f6c693f221b2c2f6ddff33841852322bfba
joekhoa <pdkhoa01.sdh19@hcmut.edu.vn>
2020-04-13 09:28:50 +0700

    add put, patch, delete api

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,15 +1,20 @@
 #   All source code
-
+# https://www.tma.vn/Hoi-dap/Cam-nang-nghe-nghiep/Con-duong-tro-thanh-cao-thu-Web-developer-ban-chon-huong-di-nao/6636
 # https://github.com/LondonAppDev/profiles-rest-api
 
 
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
-# Create your views here.
+
+from profiles_api import serializers
+from rest_framework import status
+
+
 
 class HelloApiView(APIView):
     """Test APIView"""
+    serializer_class = serializers.HelloSerializer
 
     def get(self,request,format=None):
         """Return a list of APIView features"""
@@ -24,3 +29,29 @@ class HelloApiView(APIView):
             'message':'Hello',
             'ap_apiview':an_apiview
         })
+
+    def post(self,request):
+        """ Create a hello message with our name """
+        serializer = self.serializer_class(data=request.data)
+        # Like handle of modelForm
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')    #   validated_data == cleaned_data()???
+            message = f'Hello {name}'
+            return Response({'message':message})
+        else:
+            return Response(
+                serializer.errors,
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+    def put(self,request,pk=None):
+        """Handling updating an object"""
+        return Response({'method':'PUT'})
+
+    def patch(self,request,pk=None):
+        """Handling PARTIAL update of an Object"""
+        return Response({'method':'Patch'})
+
+    def delete(self,request,pk=None):
+        """delete an object in database"""
+        return Response({'method':'DELETE'})
